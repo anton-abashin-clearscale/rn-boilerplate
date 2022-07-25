24b39a50bab9ad8ec94a717ff45838d3dfc07be5
Anton Abashin <anton.abashin@clearscale.com>
2022-07-26 00:19:09 +0400

    Rename root file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/application';
+import App from './src/app';
 import { Amplify } from 'aws-amplify';
 
 import awsConfig from './src/aws-exports';
